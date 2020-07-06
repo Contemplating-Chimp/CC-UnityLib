db0bb4cd3c2074d3fdb14e2d18287ad2c664f925
Jan Julius <jdelang@hotmail.com>
2020-07-06 13:28:46 +0200

    initial files

---
+++ CC-UnityLib.Core/Extensions/Vector2Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CC_UnityLib.Core.Extensions
+{
+    public static class Vector2Extensions
+    {
+        /// <summary>
+        /// Checks wether the given vector is in bounds of the 2D plane
+        /// </summary>
+        /// <param name="vec">Given Vector</param>
+        /// <param name="LeftTop">Left top of field</param>
+        /// <param name="BottomRight">Right Bottom of field</param>
+        /// <returns>True if the vector is within given bounds, false if not</returns>
+        public static bool IsInBounds(this Vector2 vec, Vector2 topLeft, Vector2 bottomRight)
+            => (vec.x >= topLeft.x && vec.y <= topLeft.y && vec.x <= bottomRight.x && vec.y >= bottomRight.y);
+    }
+}
---
+++ CC-UnityLib.Visual/UnityUI/ScreenTransition/ScreenTransitionManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CC_UnityLib.Visual.UnityUI.ScreenTransition
+{
+    public class ScreenTransitionManager : MonoBehaviour
+    {
+        public void Transition(ScreenTransition transition, Canvas before, Canvas after, float transitionTime)
+        {
+            GameObject emptyObject = new GameObject();
+            GameObject bgInstance = Instantiate(emptyObject);
+            foreach(GameObject child in before.transform)
+            {
+                child.transform.parent = bgInstance.transform;
+            }
+
+            GameObject agInstance = Instantiate(emptyObject);
+
+            foreach (GameObject child in after.transform)
+            {
+                child.transform.parent = agInstance.transform;
+            }
+
+            StartCoroutine(SlideToLeftTransition(bgInstance, agInstance, transitionTime));
+
+
+        }
+
+        IEnumerator SlideToLeftTransition(GameObject b, GameObject a, float tTime)
+        {
+            a.transform.position = b.transform.position + new Vector3(Screen.width, 0);
+
+            var startPosB = b.transform.position;
+            var startPosA = a.transform.position;
+
+            var targetPosB = b.transform.position - new Vector3(Screen.width, 0);
+            var targetPosA = b.transform.position;
+
+            var t = 0f;
+
+            while(t < 1)
+            {
+                t += Time.deltaTime / tTime;
+                b.transform.position = Vector3.Lerp(startPosB, targetPosB, t);
+                b.transform.position = Vector3.Lerp(startPosA, targetPosA, t);
+                yield return null;
+            }
+
+        }
+
+        public void Transition(ScreenTransition transition, GameObject before, GameObject after)
+        {
+
+        }
+    }
+}
