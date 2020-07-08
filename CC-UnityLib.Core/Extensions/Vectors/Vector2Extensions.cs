707698ee51a1f18794beb874e940fbf542d82621
Jan Julius <jdelang@hotmail.com>
2020-07-08 15:52:52 +0200

    added location for vector extensions

--- CC-UnityLib.Core/Extensions/Vectors/Vector2Extensions.cs
+++ CC-UnityLib.Core/Extensions/Vectors/Vector2Extensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace CC_UnityLib.Core.Extensions
+namespace CC_UnityLib.Core.Extensions.Vectors
 {
     public static class Vector2Extensions
     {
---
+++ CC-UnityLib.Core/Extensions/Vectors/Vector3Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CC_UnityLib.Core.Extensions.Vectors
+{
+    class Vector3Extensions
+    {
+    }
+}
