e540326bac8546468999a7acf0629c7b60277413
Jan Julius <jdelang@hotmail.com>
2020-07-20 14:41:34 +0200

    Add first steps of simpleroutine

---
+++ CC-UnityLib.Core/Coroutines/SimpleRoutine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CC_UnityLib.Core.Coroutines
+{
+    public class SimpleRoutine : MonoBehaviour, CCUnityLibCoroutine
+    {
+        List<CoroutineObject> Actions = new List<CoroutineObject>();
+
+        public void AddAction(Action action)
+        {
+            Actions.Add(new CoroutineObject(action.GetType(), action));
+        }
+
+        public void AddAction(YieldInstruction wfs)
+        {
+            Actions.Add(new CoroutineObject(wfs.GetType(), wfs));
+        }
+
+        public void Run()
+        {
+            StartCoroutine(Coroutine());
+        }
+
+        IEnumerator Coroutine()
+        {
+            foreach (var a in Actions)
+            {
+                if (a.type == typeof(WaitForSeconds))
+                    yield return (WaitForSeconds)a.action;
+                if (a.type == typeof(Action))
+                    ((Action)a.action).Invoke();
+            }
+        }
+
+        public class CoroutineObject
+        {
+            public Type type;
+            public object action;
+
+            public CoroutineObject(Type type, object action)
+            {
+                this.type = type;
+                this.action = action;
+            }
+        }
+    }
+
+}
