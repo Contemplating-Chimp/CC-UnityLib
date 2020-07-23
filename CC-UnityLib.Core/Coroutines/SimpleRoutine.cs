5579cb2a640b54ba5e9f27220119f9222516caf2
Jan Julius <jdelang@hotmail.com>
2020-07-23 11:26:39 +0200

    Add comments

--- CC-UnityLib.Core/Coroutines/SimpleRoutine.cs
+++ CC-UnityLib.Core/Coroutines/SimpleRoutine.cs
@@ -5,16 +5,51 @@ using UnityEngine;
 
 namespace CC_UnityLib.Core.Coroutines
 {
+    /// <summary>
+    /// A simple routine used to call a coroutine for quick and simple tasks.
+    /// </summary>
     public class SimpleRoutine : MonoBehaviour, CCUnityLibCoroutine
     {
-        List<CoroutineObject> Actions = new List<CoroutineObject>();
+        /// <summary>
+        /// A list of the Actions that the simpleroutine will execute
+        /// </summary>
+        public List<CoroutineObject> Actions { get; private set; } = new List<CoroutineObject>();
 
+        /// <summary>
+        /// Amount of times the <see cref="Actions"/> will loop over
+        /// </summary>
         private int _loopTimes = 1;
+        /// <summary>
+        /// Amount of times the coroutine has looped
+        /// </summary>
         private int _loopTime = 0;
-
+        /// <summary>
+        /// The position in the Coroutine <see cref="IEnumerator"/>
+        /// </summary>
         private int _position = -1;
 
+        /// <summary>
+        /// Event that is called when the routine is started
+        /// </summary>
+        public event EventHandler RoutineStarted;
+        /// <summary>
+        /// When a new action is started this event is called
+        /// </summary>
+        public event EventHandler MoveNextAction;
+        /// <summary>
+        /// When a next loop starts this event is called
+        /// </summary>
+        public event EventHandler NextLoopTime;
+        /// <summary>
+        /// When the routine is finished this event is called
+        /// </summary>
+        public event EventHandler RoutineEnded;
 
+        private void OnRoutineStarted(EventArgs e) => RoutineStarted?.Invoke(this, e);
+        private void OnMoveNextAction(EventArgs e) => MoveNextAction?.Invoke(this, e);
+        private void OnNextLoopTime(EventArgs e) => NextLoopTime?.Invoke(this, e);
+        private void OnRoutineEnded(EventArgs e) => RoutineEnded?.Invoke(this, e);
+        
         object IEnumerator.Current
         {
             get
@@ -23,6 +58,9 @@ namespace CC_UnityLib.Core.Coroutines
             }
         }
 
+        /// <summary>
+        /// The object that is currently observed in the IEnumerator
+        /// </summary>
         public object Current
         {
             get
@@ -38,6 +76,10 @@ namespace CC_UnityLib.Core.Coroutines
             }
         }
 
+        /// <summary>
+        /// Creates a routine and places this in an Empty gameobject
+        /// </summary>
+        /// <returns>Returns the routine</returns>
         public static SimpleRoutine CreateRoutine()
         {
             var obj = new GameObject();
@@ -45,25 +87,39 @@ namespace CC_UnityLib.Core.Coroutines
             return obj.GetComponent<SimpleRoutine>();
         }
 
+        /// <summary>
+        /// Adds an action
+        /// </summary>
+        /// <param name="action">The action</param>
+        /// <returns>The routine</returns>
         public SimpleRoutine AddAction(Action action)
         {
             Actions.Add(new CoroutineObject(action.GetType(), action));
             return this;
         }
 
+        /// <summary>
+        /// Adds an action (YieldInstruction) such as WaitForSeconds
+        /// </summary>
+        /// <param name="wfs">The instruction</param>
+        /// <returns>The routine/returns>
         public SimpleRoutine AddAction(YieldInstruction wfs)
         {
             Actions.Add(new CoroutineObject(wfs.GetType(), wfs));
             return this;
         }
 
+        /// <summary>
+        /// Set how many times the routine should do the set actions over. Defaults to 1.
+        /// </summary>
+        /// <param name="times">Amount of times</param>
+        /// <returns>The routine</returns>
         public SimpleRoutine LoopActions(int times)
         {
             _loopTimes = times;
             return this;
         }
-
-
+        
         public bool MoveNext()
         {
             _position++;
