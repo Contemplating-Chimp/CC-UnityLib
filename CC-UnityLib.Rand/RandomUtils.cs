1d6f874f81c9efb23746a7ac8177e040e36c49dc
Jan Julius <jdelang@hotmail.com>
2020-08-03 14:37:49 +0200

    Renamed Random to Rand and added randomutils

---
+++ CC-UnityLib.Rand/RandomUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Random = UnityEngine.Random;
+
+namespace CC_UnityLib.Rand
+{
+    public static class RandomUtils
+    {
+        public static int Range(int min, int max, int except)
+        {
+            int random = Random.Range(min, max);
+            if (random >= except)
+                random = (random + 1) % max;
+            return random;
+        }
+
+        public static int RandomExcept(int min , int max, int except)
+        {
+            int random = Random.Range(min, max);
+            if (random >= except)
+                random = (random + 1) % max;
+            return random;
+        }
+
+    }
+}
--- CC-UnityLib.Rand/Vector2Random.cs
+++ CC-UnityLib.Rand/Vector2Random.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 
-namespace CC_UnityLib.Random.Extensions
+namespace CC_UnityLib.Rand
 {
     public static class Vector2Random
     {
