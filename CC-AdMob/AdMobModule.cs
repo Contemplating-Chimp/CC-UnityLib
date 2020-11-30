cc63098cc5b819e354478db13eb169cf800dc462
Jan Julius <jdelang@hotmail.com>
2020-11-30 19:40:22 +0100

    Add: AdMob to library, banner ads added

---
+++ CC-AdMob/AdMobModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+namespace CC_AdMob
+{
+    public class AdMobModule
+    {
+        private GameObject initObject;
+        private Action<InitializationStatus> initAction;
+
+        private bool isDebugging;
+
+        private AdMobObject Initialize()
+        {
+            AdMobObject amo = initObject.AddComponent<AdMobObject>();
+            amo.initAction = this.initAction;
+            return amo;
+        }
+
+        public AdMobObject Initialize(GameObject gameobject, bool debug)
+        {
+            this.initObject = gameobject;
+            isDebugging = debug;
+            return Initialize();
+        }
+
+        public AdMobObject Initialize(GameObject gameobject, Action<InitializationStatus> actionOnInit, bool debug)
+        {
+            initObject = gameobject;
+            initAction = actionOnInit;
+            isDebugging = debug;
+            return Initialize();
+        }
+    }
+}
---
+++ CC-AdMob/AdMobObject.cs
@@ -0,0 +1,106 @@
+﻿using GoogleMobileAds.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CC_AdMob
+{
+    public class AdMobObject : MonoBehaviour
+    {
+        internal Action<InitializationStatus> initAction;
+        internal InterstitialAd interstitialAd;
+        internal BannerView bannerView;
+        internal bool debugging;
+
+        private const string debugBannerAdUnitIdAndroid = "ca-app-pub-3940256099942544/6300978111"; //test id
+        private const string debugBannerAdUnitIdIOS = "ca-app-pub-3940256099942544/2934735716";
+        private const string defaultFallBackAdUnitId = "unexpected_platform";
+        
+        private string bannerAdUnitAndroid = "";
+        private string bannerAdUnitIOS = "";
+
+        public string BannerAdUnitIdAndroid
+        {
+            set
+            {
+                bannerAdUnitAndroid = value;
+            }
+            get
+            {
+                if (debugging)
+                    return debugBannerAdUnitIdAndroid;
+                return bannerAdUnitAndroid;
+            }
+        }
+        public string BannerAdUnitIdIOS
+        {
+            set
+            {
+                bannerAdUnitIOS = value;
+            }
+            get
+            {
+                if (debugging)
+                    return debugBannerAdUnitIdIOS;
+                return bannerAdUnitIOS;
+            }
+        }
+        
+        public void Start()
+        {
+            if (initAction == null)
+                MobileAds.Initialize(initStatus => { });
+            else
+                MobileAds.Initialize(initAction);
+        }
+
+        public void RequestBanner(AdSize adSize, AdPosition adPos)
+        {
+#if UNITY_ANDROID
+            string adUnitId = BannerAdUnitIdAndroid;
+#elif UNITY_IPHONE
+            string adUnitId = BannerAdUnitIdIOS;
+#else
+            string adUnitId = defaultFallBackAdUnitId;
+#endif
+            bannerView = new BannerView(adUnitId, adSize, adPos);
+
+            bannerView.OnAdLoaded += HandleOnBannerAdLoaded;
+            bannerView.OnAdFailedToLoad += HandleOnBannerAdFailedToLoad;
+            bannerView.OnAdOpening += HandleOnBannerAdOpened;
+            bannerView.OnAdClosed += HandleOnBannerAdClosed;
+            bannerView.OnAdLeavingApplication += HandleOnBannerAdLeavingApplication;
+
+            AdRequest request = new AdRequest.Builder().Build();
+            bannerView.LoadAd(request);
+        }
+
+        public void RemoveBanner()
+        {
+            bannerView.Destroy();
+        }
+
+
+
+        #region Events
+        public virtual void HandleOnBannerAdLoaded(object sender, EventArgs e) =>
+            MonoBehaviour.print("HandleOnBannerAdLoaded event received");
+
+        public virtual void HandleOnBannerAdLeavingApplication(object sender, EventArgs e) =>
+            MonoBehaviour.print("HandleOnBannerAdLeavingApplication event received");
+
+        public virtual void HandleOnBannerAdClosed(object sender, EventArgs e) =>
+            MonoBehaviour.print("HandleOnBannerAdClosed event received");
+
+        public virtual void HandleOnBannerAdOpened(object sender, EventArgs e) =>
+            MonoBehaviour.print("HandleOnBannerAdOpened event received");
+
+        public virtual void HandleOnBannerAdFailedToLoad(object sender, AdFailedToLoadEventArgs e) =>
+            MonoBehaviour.print($"HandleOnBannerAdFailedToLoad event received with message: {e.Message}");
+
+        #endregion
+    }
+}
